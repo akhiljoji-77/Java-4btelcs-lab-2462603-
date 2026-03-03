a47925aa0412248384c1d95188107eda09e6199d
akhiljoji-77 <akhil.joji@btech.christuniversity.in>
2026-03-03 10:02:16 +0530

    Add files via upload

---
+++ Exercise1_Basics/DemoJava.java
@@ -0,0 +1,22 @@
+package  Exercise1_Basics;
+
+public class DemoJava {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("2462603");
+		int a = 10;
+		double b = 3.0;
+		char letter = 'A';
+		boolean isJavaFun = true;
+		int sum = a + (int) b;
+		double division = a / b;
+		int remainder = a % (int) b;
+		String msg = "Value of a: " + a + ", b: " + b;
+		System.out.println(msg);
+		System.out.println("sum = " + sum);
+		System.out.println("division = " + division);
+		System.out.println("remainder = " + remainder);
+		System.out.println("letter = " + letter + ", isJavaFun = " + isJavaFun);
+	}
+}
